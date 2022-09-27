6d92576318074306817deb7a5fb1377e61053856
JiseongPark1223 <87863990+JiseongPark1223@users.noreply.github.com>
2022-09-27 22:33:25 +0900

    22.09.27
    test_find_circle 문제 헤더파일 설명 보충하기

---
+++ c-Language-Study/Unit14/practice:find_rectangle_area/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int base = 20;
+    int height = 16;
+    int area;
+    
+    area = base * height / 2; //area에 base * height에 2를 나눈 값을 할당하도록 한다.
+    
+    printf("%d\n", area);
+    
+    return 0;
+}
---
+++ c-Language-Study/Unit14/test:find_circle_area/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+int main()
+{
+    float diameter;
+    float radius;
+    float area;
+    
+    scanf("%f", &diameter);
+    
+    radius = diameter/2.0;
+    
+    area = radius * radius * M_PI;
+    
+    printf("%f\n", area);
+    
+    return 0;
+}
