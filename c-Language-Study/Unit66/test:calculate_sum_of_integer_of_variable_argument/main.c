b50674a4207182f580ebea61e6360b33b9615509
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2022-11-28 20:28:50 +0900

    22.11.28

--- c-Language-Study/Unit66/test:calculate_sum_of_integer_of_variable_argument/main.c
+++ c-Language-Study/Unit66/test:calculate_sum_of_integer_of_variable_argument/main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int getSum(char *type, ...)
+int getSum(char *type, ...) //문자열을 받고 가변인자 중 정수의 합을 구하는 함수
 {
-    va_list ap;
+    va_list ap; //가변인자목록 포인터를 선언;
     int i = 0;
-    long long result = 0;
+    int result = 0; //정수 합을 구하는 변수
     
-    va_start(ap, type);
-    while (type[i] != '\0');
+    va_start(ap, type); //가변인자부터 포인터를 위치
+    while (type[i] != '\0')
     {
         switch(type[i])
         {
-            case 'i':
+            case 'i': //문자가 'i'이면 정수형 자료형 크기만큼 값을 가지고 온다음 result에 더한다.
                 result += va_arg(ap, int);
                 break;
-            case 's':
+            case 's': //문자가 's'이면  char 포인터 자료형 크기만큼 값을 가지고 온다.
                 va_arg(ap, char*);
                 break;
-            case 'd':
+            case 'd': //문자가 'd'이면 실수형 자료형 크기만큼 값을 가지고 온다.
                 va_arg(ap, double);
                 break;
             default:
@@ -26,16 +26,16 @@ int getSum(char *type, ...)
         }
         i++;
     }
-    va_end(ap);
+    va_end(ap); //포인터를 NULL로 초기화한다.
     
-    return result;
+    return result; //정수합을 반환한다.
 }
 
 int main()
 {
     int num1, num2, num3;
     
-    scanf("%d %d %d\n", &num1, &num2, &num3);
+    scanf("%d %d %d", &num1, &num2, &num3);
     
     printf("%d\n", getSum("isi", num1, "C", 10));
     printf("%d\n", getSum("sdsi", "Hello, world!", 5.3, "A", num2));
