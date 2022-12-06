9e4294346a2f485ba654c2e97c6caab2203f6aa7
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2022-12-06 22:53:19 +0900

    22.12.06
    시간이 없어 주말에 내용 이해 및 커밋 보충

--- c-Language-Study/Unit69/test:printf_personal_information/main.c
+++ c-Language-Study/Unit69/test:printf_personal_information/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-struct Person {
+struct Person { //인적사항이 저장될 Person구조체를 선언
     char name[30];
     int age;
-    void (*print)(struct Person *);
+    void (*print)(struct Person *); 
 };
 
-void print(struct Person *p)
+void print(struct Person *p) //포인터 변수를 받아 인적사항을 출력할 함수
 {
     printf("%s %d\n", p->name, p->age);
 }
  
-void executer(void (*fp[])(struct Person *), struct Person p[], int count)
+void executer(void (*fp[])(struct Person *), struct Person p[], int count) 
 {
     for (int i = 0; i < count; i++)
     {
