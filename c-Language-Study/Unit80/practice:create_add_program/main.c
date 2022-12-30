cb82940cf3151776d52070c5886b69135c9d838d
Ji seong Park <87863990+JiseongPark1223@users.noreply.github.com>
2022-12-30 20:13:51 +0900

    22.12.30

---
+++ c-Language-Study/Unit80/practice:create_add_program/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) //옵션의 개수와 옵션 문자열을 배열로 받는다.
+{
+    int result = 0;
+    
+    for (int i = 1; i < argc; i++) 옵션의 개수만큼 반복한 뒤 배열의 값을 더한다.
+    {
+        result += atoi(argv[i]);
+    }
+    
+    printf("%d\n", result);
+    
+    return 0;
+}
